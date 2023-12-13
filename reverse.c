b80dbc33abfc68a4b01b72a609bf5c55b9917517
Otto Oikarinen <otto@oikarinenotto.fi>
2023-12-13 14:26:29 +0200

    This program is now working, maybe little tweaks.

--- reverse.c
+++ reverse.c
@@ -1,11 +1,75 @@
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
+typedef struct list {
+    struct list *pPrevious;
+    char *pString;
+    struct list *pNext;
+} LIST;
+
+void printList(LIST *pHead, FILE *output) {
+    int first = 0;
+
+    printf("Printing the list to the chosen output.\n\n");
+    LIST *pCurrent = pHead;
+    while (pCurrent->pNext != NULL) {
+        pCurrent = pCurrent->pNext;
+    }
+    while (pCurrent != NULL) {
+        fprintf(output, "%s", pCurrent->pString);
+        if (first == 0 && output != stdout) {
+            fprintf(output, "\n");
+            first = 1;
+        }
+        pCurrent = pCurrent->pPrevious;
+    }
+    printf("\n");
+}
+
 void reverseFile(FILE *input, FILE *output) {
-    fprintf(output, "Testiteksti\n");
-};
+    char *pLine = NULL;
+    size_t dLineLenght = 0;
+    ssize_t nread;
+
+    LIST *pHead = NULL;
+    LIST *pCurrent = NULL;
+
+    while ((nread = getline(&pLine, &dLineLenght, input)) != -1) {
+        printf("Retrieved line of length %zd.\n", nread);
+
+        if (output == stdout && strcmp(pLine, "quit\n") == 0) {
+            printf("Stopping.\n");
+            break;
+        }
+        
+        LIST *newNode = (LIST *)malloc(sizeof(LIST));
+        if (newNode == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
+            exit(1);
+        }
+
+        newNode->pString = strdup(pLine);
+        if (newNode->pString == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
+            free(newNode);
+            exit(1);
+        }
+
+        if (pHead == NULL) {
+            pHead = newNode;
+            pCurrent = newNode;
+        } else {
+            pCurrent->pNext = newNode;
+            newNode->pPrevious = pCurrent;
+            pCurrent = newNode;
+        }
+    }
+    printList(pHead, output);
+}
+
 
 int main(int argc, char *argv[]) {
     if(argc > 3) {
