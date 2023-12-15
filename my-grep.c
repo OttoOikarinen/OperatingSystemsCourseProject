15954bae44cd46243cea957c0f996ad20841f2a9
Otto Oikarinen <otto@oikarinenotto.fi>
2023-12-15 15:48:54 +0200

    Made the program accept lines from terminal

--- my-grep.c
+++ my-grep.c
@@ -1,9 +1,33 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void grepFile(FILE *input, char *cSearchTerm) {
-    printf("%s\n", cSearchTerm);
+    char* pLine = NULL;
+    size_t iLineLenght = 0;
+    ssize_t nread;
+
+    if (input == stdin) {
+        printf("Write your inputs. When you want to stop, write 'quit'.\n");
+
+    }
+
+    // For every line in file the program will just print it.
+    while ((nread = getline(&pLine, &iLineLenght, input)) != -1) {
+        char *result = strstr(pLine, cSearchTerm);
+        // If user writes 'quit' in terminal, stops getting inputs.
+        if (input == stdin && strcmp(pLine, "quit\n") == 0) {
+            printf("Stopping.\n\n");
+            break;
+        }
+        if (result != NULL) {
+            printf("%s", pLine);
+        }
+    }
+
+    free(pLine);
 }
 
 int main(int argc, char *argv[]) {
@@ -15,7 +39,10 @@ int main(int argc, char *argv[]) {
 
     char *cSearchTerm = argv[1];
 
-    for (int i = 2; i < argc; i++) { // For every parameter given.
+    if (argc == 2) {
+        grepFile(stdin, cSearchTerm);
+    } else {
+        for (int i = 2; i < argc; i++) { // For every parameter given.
         FILE *input = fopen(argv[i], "r"); // Opens input to chosen file.
         if (!input) { // If fopen did not succeed:
             fprintf(stderr, "my-grep: cannot open file\n");
@@ -26,5 +53,6 @@ int main(int argc, char *argv[]) {
         grepFile(input, cSearchTerm);
         fclose(input);
     }
+    }// searchterm for testing :D
     return 0;
 }
